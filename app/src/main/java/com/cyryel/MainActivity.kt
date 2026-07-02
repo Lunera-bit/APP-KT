f143b4ad8aaa3a85c3956a6aa16e41d363abc541
Lunera-bit <victormanuellunalopez31@gmail.com>
2026-07-02 01:10:32 -0500

    revert: quitado status bar color

--- app/src/main/java/com/cyryel/MainActivity.kt
+++ app/src/main/java/com/cyryel/MainActivity.kt
@@ -7,11 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
-import androidx.core.view.WindowCompat
 import androidx.navigation.compose.rememberNavController
 import com.cyryel.navigation.AppNavGraph
 import com.cyryel.ui.theme.TiendaCyryelTheme
@@ -23,10 +19,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            SideEffect {
-                window.statusBarColor = Color(0xFF1A3A6B).toArgb()
-                WindowCompat.getInsetsController(window, window.decorView).isAppearanceLightStatusBars = false
-            }
             TiendaCyryelTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { _ ->
                     val navController = rememberNavController()
